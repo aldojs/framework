c63f4198734a2136f3c9637779c334b3ad9d5caa
absolux <a.mouhassine@gmail.com>
2018-05-13 19:04:51 +0000

    Add server tests

---
+++ packages/http/test/server/factory.ts
@@ -0,0 +1,31 @@
+
+import 'mocha'
+import * as http from 'http'
+import * as https from 'https'
+import * as assert from 'assert'
+import { noop } from './_support'
+import { createServer, Server } from '../../src'
+
+describe('createServer()', () => {
+  it('should be a function', () => {
+    assert.equal(typeof createServer, 'function')
+  })
+
+  it ('should return an instance of `Server`', () => {
+    assert(createServer(noop as any) instanceof Server)
+  })
+
+  it('should set the listener', () => {
+    let server = createServer(noop as any)
+
+    assert.equal(server.native.listenerCount('request'), 1)
+  })
+
+  describe('when `tls` options are provided', () => {
+    it('should create a HTTPS server', () => {
+      var server = createServer(noop as any, { tls: {} })
+
+      assert(server.native instanceof https.Server)
+    })
+  })
+})
---
+++ packages/http/test/server/start.ts
@@ -0,0 +1,39 @@
+
+import 'mocha'
+import * as assert from 'assert'
+import { Server } from '../../src'
+import { createHttpServerStub } from './_support'
+
+describe('server.start(argument)', () => {
+  var stub: any
+  var server: Server
+
+  beforeEach(() => {
+    stub = createHttpServerStub()
+    server = new Server(stub)
+  })
+
+  it('should return a promise', () => {
+    var result = server.start(123)
+
+    assert(result instanceof Promise)
+  })
+
+  describe('when `argument` is a port number', () => {
+    it('should pass the port number to `listen` method', () => {
+      server.start(123)
+
+      assert(stub.listen.calledOnceWith(123))
+    })
+  })
+
+  describe('when `argument` is a plain object', () => {
+    it('should pass the options to `listen` method', () => {
+      var options = { port: 123, host: 'localhost' }
+
+      server.start(options)
+
+      assert(stub.listen.calledWithMatch(options))
+    })
+  })
+})
